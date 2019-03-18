e72e141031dc363f24c3dc98ece4f3c45bff1667
Phung Thang <phunglienhuy1810@gmail.com>
2019-03-18 15:36:11 +0700

    Huy 3.18.2019 3.35PM

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { StorageService} from '../app/services/storage.service';
+
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
@@ -16,6 +18,7 @@ import { AppComponent } from './app.component';
   providers: [
     StatusBar,
     SplashScreen,
+    StorageService,
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
   ],
   bootstrap: [AppComponent]
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -3,6 +3,7 @@ import*as firebase from 'firebase';
 import {FIREBASE_CONFIG ,getdata} from '../config_firebase';
 import { Router } from '@angular/router';
 import { Subject } from 'rxjs';
+import { StorageService,item} from '../services/storage.service';
 
 @Component({
   selector: 'app-login',
@@ -17,7 +18,9 @@ export class LoginPage implements OnInit {
   listStudent=[];
   selectedProduct : Subject<any> = new Subject;
 
-  constructor(private router: Router) { 
+  user:item=<item>{};
+
+  constructor(private router: Router, private storage: StorageService) { 
     this.btn_getDocument();
   }
 
@@ -26,20 +29,43 @@ export class LoginPage implements OnInit {
   btn_login(){
     // console.log(this.edt_email);
     // console.log(this.edt_pass);
-    let ref =firebase.database().ref('account/').orderByChild("email").equalTo(this.edt_email);
-    ref.on('value',resp=>{
-      this.account=getdata(resp);
-      if(this.account.length>0){
-        if(this.account[0].pass===this.edt_pass){
-          console.log("Conguration")
-          this.router.navigate(['']);
-        }else{
-          console.log("wrong password")
-        }
-      }else{
-        console.log("tai khoan khong ton tai")
-      }
+    // let ref =firebase.database().ref('account/').orderByChild("email").equalTo(this.edt_email);
+    // ref.on('value',resp=>{
+    //   this.account=getdata(resp);
+    //   if(this.account.length>0){
+    //     if(this.account[0].pass===this.edt_pass){
+    //       console.log("Conguration")
+    //       this.router.navigate(['']);
+    //     }else{
+    //       console.log("wrong password")
+    //     }
+    //   }else{
+    //     console.log("tai khoan khong ton tai")
+    //   }
+    // })
+    // let Object={
+    //   address: "sdfds",
+    //   diem :{
+    //     dia:10,
+    //     toan:1,
+    //     ly:2
+    //   },
+    //   email :"abc@gmail.com",
+    //   monhoc :{
+    //     mon1:"toan",
+    //     mon2:"ly"
+    //   },
+    //   name :"Trinh Thang Binh",
+    //   pass :"123456"
+    // }
+    // firebase.database().ref('account/').push({});
+    this.user.email="abc@gmail.com";
+    this.user.pass="123";
+    this.storage.addItem(this.user).then(item=>{
+      console.log(item)
     })
+    
+
   }
 
     btn_getDocument(){

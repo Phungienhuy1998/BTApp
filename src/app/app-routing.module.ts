2532a9b1a962fbbed36d18b4d795883eeeff5f87
lehongphuc <you@example.com>
2019-03-22 14:29:26 +0700

    lehongphuc22/3/2019

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-<<<<<<< HEAD
+
   { path: 'tab1', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: '', loadChildren: './login/login.module#LoginPageModule' ,pathMatch: 'full'},
-=======
+
   { path: '', loadChildren: './tabs/tabs.module#TabsPageModule' },
   { path: 'login', loadChildren: './login/login.module#LoginPageModule' },
->>>>>>> a2eaf01ad43f1a64dfbf1d437cdcceefb0228dc8
+
   { path: 'thongtin', loadChildren: './thongtin/thongtin.module#ThongtinPageModule' },
   { path: 'ketqua', loadChildren: './ketqua/ketqua.module#KetquaPageModule' },
   { path: 'lichhoc', loadChildren: './lichhoc/lichhoc.module#LichhocPageModule' },
@@ -17,10 +17,7 @@ const routes: Routes = [
 
 
 
-<<<<<<< HEAD
-=======
 
->>>>>>> a2eaf01ad43f1a64dfbf1d437cdcceefb0228dc8
 ];
 @NgModule({
   imports: [
--- src/app/chitiet/chitiet.page.ts
+++ src/app/chitiet/chitiet.page.ts
@@ -1,18 +1,37 @@
 import { Component, OnInit } from '@angular/core';
-import {ServiceService} from '../services/service.service'
+import { ServiceService } from '../services/service.service'
+import { Storage } from '@ionic/storage';
+import { Subject } from 'rxjs';
 @Component({
   selector: 'app-chitiet',
   templateUrl: './chitiet.page.html',
   styleUrls: ['./chitiet.page.scss'],
 })
 export class ChitietPage implements OnInit {
+  liststudent: [];
 
-  constructor(private _Service:ServiceService) { }
+  constructor(private _Service: ServiceService, private storage: Storage) { }
 
   ngOnInit() {
-   
+    this.calldata();
   }
-goBack(){
-  this._Service.goBack();
-}
+
+  calldata() {
+    this.storage.get('MY_ACCOUNT').then((val) => {
+      console.log('Account is', val);
+      this.liststudent = val;
+      console.log(this.liststudent);
+    });
+  }
+
+  logoutdata() {
+    this.storage.remove('MY_ACCOUNT').then((val) => {
+      console.log('Account is', val);
+      this.liststudent = val;
+    });
+  }
+
+  goBack() {
+    this._Service.goBack();
+  }
 }
